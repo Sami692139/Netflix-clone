f4e7d0fdf48c49930e5a98ad4b93a1064f19fe61
sami692139 <samuel.tassew2020@gmail.com>
2024-11-07 22:07:21 +0300

    editing footer css

--- netflix-clone/src/components/Footer/Footer.jsx
+++ netflix-clone/src/components/Footer/Footer.jsx
@@ -13,42 +13,41 @@ const Footer = () => {
           <InstagramIcon />
           <YouTubeIcon />
         </div>
-      </div>
-      <div className="footer_data">
-        <div>
-          <ul>
-            <li>Audio Description</li>
-            <li>Investor Relations</li>
-            <li>Legal Notice</li>
-          </ul>
-        </div>
-        <div>
+
+        <div className="footer_data">
+          <div>
             <ul>
-                <li>Help Center</li>
-                <li>Jobs</li>
-                <li>Cookie Preferences</li>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
             </ul>
-        </div>
-        <div>
+          </div>
+          <div>
             <ul>
-                <li>Gift Cards</li>
-                <li>Terms of Use</li>
-                <li>Corporate Information</li>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
             </ul>
-        </div>
-        <div>
+          </div>
+          <div>
             <ul>
-                <li>Media Center</li>
-                <li>Privacy</li>
-                <li>Contact Us</li>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
             </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
-        <div className='server_code'>
-        <p>Service Code</p>
+        <div className="server_code">
+          <p>Service Code</p>
         </div>
-        <div className="copy-write">
-            &copy; 1997-2024 Netflix, Inc
-        </div>
+        <div className="copy-write">&copy; 1997-2024 Netflix, Inc</div>
       </div>
     </div>
   );
